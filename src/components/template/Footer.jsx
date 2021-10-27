7f967f14aebb64928af4dc4399914ef0069ca9d6
Matheus Couto <matheuscoutoferreira@hotmail.com>
2021-10-27 17:15:49 -0300

    main

---
+++ src/components/template/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+import React from "react";
+
+export default (props) => (
+  <footer className="footer">
+    <span>
+      Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+      <strong>
+        {" "}
+        <i class="fa fa-code bluecolor"></i> Coudev
+      </strong>
+    </span>
+  </footer>
+);
