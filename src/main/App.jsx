4f0963b44dab6dd275a575a802ed848be5af94dc
Matheus Couto <matheuscoutoferreira@hotmail.com>
2021-10-28 00:47:27 -0300

    Começando componente cadastro usuário

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -2,22 +2,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 import "./App.css";
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+/*Pode ser também a estratégia HashRouter em ves de BrowserRouter */
 
 import Logo from "../components/template/Logo";
 import Nav from "../components/template/Nav";
-import Main from "../components/template/Main";
+import Routes from "./Routes";
 import Footer from "../components/template/Footer";
-import Header from "../components/template/Header";
 
 export default (props) => (
-  <div className="app">
-    <Logo />
-    <Nav />
-    <Main
-      icon="home"
-      title="Início"
-      subtitle="Segundo Projeto do Capítulo React"
-    />
-    <Footer />
-  </div>
+  <BrowserRouter>
+    <div className="app">
+      <Logo />
+      <Nav />
+      <Routes />
+      <Footer />
+    </div>
+  </BrowserRouter>
 );
